0639fd255a0d449ea06e53ecca34d593914544b7
ilham sya <ilsya@lfbn-ren-1-496-27.w2-10.abo.wanadoo.fr>
2024-03-10 14:35:03 +0700

    Add new function landing page

--- routes/web.php
+++ routes/web.php
@@ -49,9 +49,9 @@ Route::get('/google/callback', function () {
     return redirect(session('url.intended', RouteServiceProvider::HOME));
 });
 
-Route::get('/email/verify', function () {
-    return view('auth.verify-email');
-})->middleware('auth')->name('verification.notice');
+// Route::get('/email/verify', function () {
+//     return view('auth.verify-email');
+// })->middleware('auth')->name('verification.notice');
 
 Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
     $request->fulfill();
