11d5d06c5451e1ae6193f230224d9ed96297359f
ilham sya <ilsya@lfbn-ren-1-496-36.w2-10.abo.wanadoo.fr>
2024-03-11 16:36:40 +0700

    fix:collback midtrands

--- routes/api.php
+++ routes/api.php
@@ -20,5 +20,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('midtras-callback', [OrderController::class, 'store']);
+Route::post('midtras-callback', [OrderController::class, 'callback']);
 Route::post('doctors/store', [DoctorController::class, 'store'])->name('doctor-store');
