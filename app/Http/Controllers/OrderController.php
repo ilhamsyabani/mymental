079b987922fa756ef93a89b17a3ea46d40029142
ilham sya <ilsya@lfbn-ren-1-496-36.w2-10.abo.wanadoo.fr>
2024-03-11 17:11:58 +0700

    fix:collback midtrands

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -77,7 +77,7 @@ class OrderController extends Controller
         $hashed = hash("sha512", $request->order_id.$request->status_code.$request->gross_amount.$serverKey);
         if($hashed == $request->signature_key){
             if($request->transaction_status == 'capture'){
-                $order = Order::where('kode', $request->oreder_id);
+                $order = Order::where('kode', $request->order_id)->first();
                 dd($order);
                 $order->update(['status' => 'Paid']);
             }
